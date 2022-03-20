f3d4bfe2a785c529606769dc7fe6f56b60c163bd
AnuragSrivastava84 <anurag1984.srivas@gmail.com>
2022-03-20 13:17:36 +0800

    Set up the skeleton of the project
    * Changes to set up the skeleton of the project

---
+++ src/main/java/com/spring/security/oauth/SpringSecurityOuthGitHubLoginApplication.java
@@ -0,0 +1,13 @@
+package com.spring.security.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOuthGitHubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOuthGitHubLoginApplication.class, args);
+	}
+
+}
